2566817cc1cfcddfd5bac89084424c52f6232b58
Andy <andy.park@nafinc.com>
2019-08-12 18:26:58 -0700

    Temp push

--- Test/__tests__/App-test.js
+++ Test/__tests__/App-test.js
@@ -5,10 +5,35 @@
 import 'react-native';
 import React from 'react';
 import App from '../../App/src/App';
+import { shallow } from 'enzyme';
+
+jest.mock('../../App/src/Route', ()=>(
+  {createAppContainer: "Hello"}))
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { JestEnvironment } from '@jest/environment';
 
+describe('Rendering', ()=>{
+  let component, instance;
+  beforeEach( () =>{
+     component = shallow(<App />);
+     instance = component.instance();
+  })
+
+  // it('renders the component', () => {
+  //   const wrapper = shallow(<App />);
+  //   expect(wrapper.find(Foo)).to.have.lengthOf(3);
+  // });
+  
+  it('it should be defined', ()=>{
+    expect(instance).toBeDefined();
+  })
+
+  it('class should include Header', ()=>{
+    expect(component.find(".header"))
+  })
+})
 it('renders correctly', () => {
   renderer.create(<App />);
 });
---
+++ Test/cypress/integration/app_spec.js
@@ -0,0 +1,10 @@
+import App from '../../../App/src/App';
+
+describe('Testing App', () => {
+    it('', () => {
+      expect(true).to.equal(true)
+    })
+    it('Does not do much!', function() {
+        expect(true).to.equal(false)
+      })
+  })
